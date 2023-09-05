2b00186eb628b44610d7c05c2af5525a3e7fc6f6
sonia.restuccia <sonia.restuccia@capgemini.com>
2023-09-05 11:47:25 +0200

    created FornitoreDTO

---
+++ src/main/java/it/popso/popsogift/dto/FornitoreDTO.java
@@ -0,0 +1,18 @@
+package it.popso.popsogift.dto;
+
+import lombok.Data;
+
+@Data
+public class FornitoreDTO {
+
+    private String ragioneSociale;
+    private String email;
+    private String indirizzo;
+    private String stato;
+    private String cap;
+    private String continente;
+    private String telefono;
+    private String citta;
+    private String provincia;
+    private String partitaIva;
+}

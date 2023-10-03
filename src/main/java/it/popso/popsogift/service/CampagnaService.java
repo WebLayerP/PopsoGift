cd5d3cfa697c17678bf4f0b1d2c4c0c78c1f98d8
Bujar_Subashi <bujar.subashi@capgemini.com>
2023-10-03 16:41:26 +0200

    Fix: logica inserimento filiali in nuova campagna

--- src/main/java/it/popso/popsogift/service/CampagnaService.java
+++ src/main/java/it/popso/popsogift/service/CampagnaService.java
@@ -3,10 +3,12 @@ package it.popso.popsogift.service;
 
 import it.popso.popsogift.dto.CampagnaDTO;
 import it.popso.popsogift.entity.Campagna;
+import it.popso.popsogift.entity.Filiale;
 import it.popso.popsogift.mapper.CampagnaMapper;
 import it.popso.popsogift.mapper.StatoMapper;
 import it.popso.popsogift.mapper.TipologiaMapper;
 import it.popso.popsogift.repository.CampagnaRepository;
+import it.popso.popsogift.repository.FilialeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +27,9 @@ public class CampagnaService {
     private TipologiaMapper tipologiaMapper;
 
     @Autowired
+    private FilialeRepository filialeRepository;
+
+    @Autowired
     private StatoMapper statoMapper;
 
     public List<Campagna> getAllCampagne() {
@@ -35,8 +40,12 @@ public class CampagnaService {
         Campagna campagna = campagnaMapper.campagnaDTOToEntity(campagnaDTO);
         campagna.setTipologia(tipologiaMapper.getTipologia(campagnaDTO));
         campagna.setStato(statoMapper.getStato(campagnaDTO));
+        for (Filiale filiale: campagna.getListaFiliali()) {
+            System.out.println(filialeRepository.existsById(filiale.getCodiceFiliale()));
+            if (!filialeRepository.existsById(filiale.getCodiceFiliale()))
+                filialeRepository.save(filiale);
+        }
         Campagna campagnaInserita = campagnaRepository.save(campagna);
         return campagnaInserita;
     }
-
 }

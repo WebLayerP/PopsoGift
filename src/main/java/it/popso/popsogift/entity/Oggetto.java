42da6ce380f052896d60d277a066ad62fbb0784c
sonia.restuccia <sonia.restuccia@capgemini.com>
2023-11-20 15:21:41 +0100

    fix FilialeMapper e Oggetto getters and setters

--- src/main/java/it/popso/popsogift/entity/Oggetto.java
+++ src/main/java/it/popso/popsogift/entity/Oggetto.java
@@ -2,13 +2,11 @@ package it.popso.popsogift.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
-import lombok.Data;
 
 import java.util.Date;
 
 @Entity
 @Table(name ="OGGETTO")
-@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Oggetto {
     @Id
@@ -37,5 +35,85 @@ public class Oggetto {
     @ManyToOne
     @JoinColumn(name="TIPOLOGIA")
     private TipologiaOggetto tipologiaOggetto;
+
+    public Integer getIdOggetto() {
+        return idOggetto;
+    }
+
+    public void setIdOggetto(Integer idOggetto) {
+        this.idOggetto = idOggetto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(Double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public void setCodice(String codice) {
+        this.codice = codice;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Fornitore getFornitore() {
+        return fornitore;
+    }
+
+    public void setFornitore(Fornitore fornitore) {
+        this.fornitore = fornitore;
+    }
+
+    public Date getDataAggiornamento() {
+        return dataAggiornamento;
+    }
+
+    public void setDataAggiornamento(Date dataAggiornamento) {
+        this.dataAggiornamento = dataAggiornamento;
+    }
+
+    public Date getDataInserimento() {
+        return dataInserimento;
+    }
+
+    public void setDataInserimento(Date dataInserimento) {
+        this.dataInserimento = dataInserimento;
+    }
+
+    public TipologiaOggetto getTipologiaOggetto() {
+        return tipologiaOggetto;
+    }
+
+    public void setTipologiaOggetto(TipologiaOggetto tipologiaOggetto) {
+        this.tipologiaOggetto = tipologiaOggetto;
+    }
 
 }
--- src/main/java/it/popso/popsogift/mapper/FilialeMapper.java
+++ src/main/java/it/popso/popsogift/mapper/FilialeMapper.java
@@ -3,6 +3,7 @@ package it.popso.popsogift.mapper;
 import it.popso.popsogift.dto.FilialeDTO;
 import it.popso.popsogift.entity.Filiale;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValueCheckStrategy;
 
 import java.util.List;
@@ -10,10 +11,12 @@ import java.util.List;
 @Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface FilialeMapper {
 
+    @Mapping(target="listaBeneficiari", ignore = true)
     FilialeDTO filialeToFilialeDTO(Filiale filiale);
-
+    @Mapping(target="listaBeneficiari", ignore = true)
     Filiale filialeDTOToFiliale(FilialeDTO filialeDTO);
-
+    @Mapping(target="listaBeneficiari", ignore = true)
     List<Filiale> listaFilialeDTOToEntity(List<FilialeDTO> filialeDTO);
+    @Mapping(target="listaBeneficiari", ignore = true)
     List<FilialeDTO> listaFilialeToDTO(List<Filiale> filiale);
 }

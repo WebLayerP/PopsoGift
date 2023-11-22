969c4a49a9e9b7ec971843755ea72055ded03b71
sonia.restuccia <sonia.restuccia@capgemini.com>
2023-11-22 15:56:58 +0100

    metodi default per la categoria aggiunti

--- src/main/java/it/popso/popsogift/mapper/FornitoreMapper.java
+++ src/main/java/it/popso/popsogift/mapper/FornitoreMapper.java
@@ -3,6 +3,7 @@ package it.popso.popsogift.mapper;
 import it.popso.popsogift.dto.FornitoreDTO;
 import it.popso.popsogift.dto.OggettoDTO;
 import it.popso.popsogift.dto.TipologiaOggettoDTO;
+import it.popso.popsogift.entity.Categoria;
 import it.popso.popsogift.entity.Fornitore;
 import it.popso.popsogift.entity.Oggetto;
 import it.popso.popsogift.entity.TipologiaOggetto;
@@ -25,5 +26,13 @@ public interface FornitoreMapper {
 
     default TipologiaOggettoDTO mapTipologiaOggettoDTOtoTipologia(TipologiaOggetto tipologiaOggetto){
         return null!=tipologiaOggetto ? tipologiaOggetto.getNomeTipologia(): null;
+    }
+    default Categoria toCategoria(String categoriaString){
+        Categoria categoria = new Categoria();
+        categoria.setNomeCategoria(categoriaString.toUpperCase());
+        return categoria;
+    }
+    default String mapCategoriaDTOtoCategoria(Categoria categoria){
+        return categoria.getNomeCategoria();
     }
 }

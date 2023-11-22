d16d3792697645c002ab2c6bb17c42e9cd0d4b0c
sonia.restuccia <sonia.restuccia@capgemini.com>
2023-11-22 15:55:46 +0100

    query existsByNomeCategoria added

--- src/main/java/it/popso/popsogift/repository/CategoriaRepository.java
+++ src/main/java/it/popso/popsogift/repository/CategoriaRepository.java
@@ -4,4 +4,6 @@ import it.popso.popsogift.entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
+
+    boolean existsByNomeCategoria(String nomeCategoria);
 }

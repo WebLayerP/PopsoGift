981eb1670f0cbb7c07c948b25b359be49cd79348
sonia.restuccia <sonia.restuccia@capgemini.com>
2023-11-22 15:55:10 +0100

    fix Categoria e CategoriaDTO

--- src/main/java/it/popso/popsogift/dto/CategoriaDTO.java
+++ src/main/java/it/popso/popsogift/dto/CategoriaDTO.java
@@ -8,6 +8,5 @@ import java.io.Serializable;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class CategoriaDTO implements Serializable {
-    private Integer idCategoria;
     private String nomeCategoria;
 }
--- src/main/java/it/popso/popsogift/entity/Categoria.java
+++ src/main/java/it/popso/popsogift/entity/Categoria.java
@@ -10,8 +10,6 @@ import lombok.Data;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Categoria {
     @Id
-    @Column(name="ID_CATEGORIA")
-    private Integer idCategoria;
     @Column(name="DESCRIZIONE")
     private String nomeCategoria;
 }
